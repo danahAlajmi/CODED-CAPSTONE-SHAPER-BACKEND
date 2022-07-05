c923638f890f16158523cb12ca6e4a14e8de3304
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-07-05 21:57:38 +0300

    Fixed sign up and profile interaction

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,3 +1,4 @@
+
 const Session = require("../../models/Session");
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
@@ -46,19 +47,20 @@ exports.signup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
+    const newProfile = await Profile.create({ user: newUser._id });
+    await User.findByIdAndUpdate(newUser._id, { profile: newProfile._id });
+    
     const payload = {
       _id: newUser._id,
       username: newUser.username,
       email: newUser.email,
       isTrainer: newUser.isTrainer,
-      profile: newUser.profile,
+      profile: newProfile._id,
     };
-
+    
     const token = jwt.sign(payload, process.env.JWT_SECRET);
-
-    const newProfile = await Profile.create({ user: newUser._id });
-    await User.findByIdAndUpdate(newUser._id, { profile: newProfile._id });
-
+    
+    
     res.json({ token: token });
   } catch (err) {
     res.status(500).json("Server Error");
