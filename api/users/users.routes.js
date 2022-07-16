bf048262106e64c80a2b9eaaef07ca65842a4fb6
Msdalmajdi <msdalmajdi@hotmail.com>
2022-07-16 23:01:11 +0300

    Initiate excel reports

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const { signup, getUsers, getTrainers, signin } = require("./users.controllers");
+const {
+  signup,
+  getUsers,
+  getTrainers,
+  signin,
+  getReport,
+} = require("./users.controllers");
 
 router.get("/", getUsers);
 router.get("/trainers", getTrainers);
 router.post("/signup", signup);
 router.post(
-    "/signin",
-    passport.authenticate("local", { session: false }),
-    signin
-  );
+  "/signin",
+  passport.authenticate("local", { session: false }),
+  signin
+);
 
+router.post("/report", getReport);
 module.exports = router;
